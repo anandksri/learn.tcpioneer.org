1cd63ec5ddc9c547494834faee42281320f56a67
anandksri <anandkeshari0711@gmail.com>
2026-02-28 04:27:37 -0600

    under construction

--- components/markdown.js
+++
@@ -1,7 +0,0 @@
-async function loadMarkdown(file) {
-  const res = await fetch(file);
-  const text = await res.text();
-
-  document.getElementById('content').innerHTML =
-    marked.parse(text);
-}
